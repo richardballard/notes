fba4387aa7bbb570e425f058120c4d47a629cd41
Richard Ballard <richard.ballard@dam-digital.com>
2018-10-02 14:09:17 +0100

    Updated packages and ran gulp tasks

--- Tasks/scssWatch.js
+++ Tasks/scssWatch.js
@@ -7,7 +7,6 @@ const scssWatch = () => {
         `${config.src.scss}${config.selectors.scss}`,
     ], ['scssBuild']);
     gutil.log(process.version);
-    gutil.log("You must be running on node version 6.1.x or higher for the compiler to work. Note: Visual Studio may need to path to your version of node, otherwise it defaults to an older version, Follow this link for more details: https://stackoverflow.com/questions/43849585/update-node-version-in-visual-studio-2017");
 };
 
 gulp.task('scssWatch', scssWatch);
